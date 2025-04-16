b216532a43c0e12fea10d9416cfe8f3163315ad8
Murli Goel <murligoel@Murlis-MacBook-Pro.local>
2025-04-16 12:23:46 +0530

    set up for factory design pattern

---
+++ factory-design-pattern/src/main/java/com/practice/factorydesignpattern/FactoryDesignPatternApplication.java
@@ -0,0 +1,13 @@
+package com.practice.factorydesignpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactoryDesignPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FactoryDesignPatternApplication.class, args);
+    }
+
+}
---
+++ factory-design-pattern/src/test/java/com/practice/factorydesignpattern/FactoryDesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.factorydesignpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactoryDesignPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
