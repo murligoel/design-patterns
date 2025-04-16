0223054dcacefcef256882a5b5b370d46ea401fa
Murli Goel <murligoel@Murlis-MacBook-Pro.local>
2025-04-16 12:56:30 +0530

    added abstract factory design pattern

---
+++ abstract-factory-design-pattern/src/main/java/com/practice/abstractfactorydesignpattern/AbstractFactoryDesignPatternApplication.java
@@ -0,0 +1,13 @@
+package com.practice.abstractfactorydesignpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbstractFactoryDesignPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbstractFactoryDesignPatternApplication.class, args);
+    }
+
+}
