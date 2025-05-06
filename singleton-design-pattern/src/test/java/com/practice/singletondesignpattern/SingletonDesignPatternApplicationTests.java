6e030bb804d285be15c24bd302e47a1d34686979
Murli Goel <murligoel@Murlis-MacBook-Pro.local>
2025-05-06 20:19:54 +0530

    setup for singleton design pattern

---
+++ singleton-design-pattern/src/test/java/com/practice/singletondesignpattern/SingletonDesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.singletondesignpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingletonDesignPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
